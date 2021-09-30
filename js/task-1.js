2dae614bbf46bdc1e39d9ef1ea9f79c202efb1ee
MaryaSamsonenko <84477591+MaryaSamsonenko@users.noreply.github.com>
2021-09-30 21:34:47 +0300

    добавляет решение первых двух задач

--- js/task-1.js
+++ js/task-1.js
@@ -1,17 +1,25 @@
 // Напиши функцию-конструктор Account, которая создает объект со свойствами login и email. В prototype функции-конструктора добавь метод getInfo(), который выводит в консоль значения полей login и email объекта который его вызвал.
 
-// console.log(Account.prototype.getInfo); // function
+const Account = function ({ login, email } = {}) {
+    this.login = login;
+    this.email = email;
+};
+Account.prototype.getInfo = function (login, email) {
+    console.log(`login:  ${this.login}, email:  ${this.email}`);
+};
 
-// const mango = new Account({
-//   login: 'Mangozedog',
-//   email: 'mango@dog.woof',
-// });
+console.log(Account.prototype.getInfo); // function
 
-// mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+const mango = new Account({
+    login: 'Mangozedog',
+    email: 'mango@dog.woof',
+});
 
-// const poly = new Account({
-//   login: 'Poly',
-//   email: 'poly@mail.com',
-// });
+mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
 
-// poly.getInfo(); // Login: Poly, Email: poly@mail.com
+const poly = new Account({
+    login: 'Poly',
+    email: 'poly@mail.com',
+});
+
+poly.getInfo(); // Login: Poly, Email: poly@mail.com
