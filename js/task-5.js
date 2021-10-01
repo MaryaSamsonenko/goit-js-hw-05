834e82244c031261922946c93603b4b29ad92a9b
MaryaSamsonenko <84477591+MaryaSamsonenko@users.noreply.github.com>
2021-10-01 19:38:35 +0300

    добавляет решение 5 задачи

--- js/task-5.js
+++ js/task-5.js
@@ -1,6 +1,4 @@
 // Напиши класс Car с указанными свойствами и методами.
-
-// class Car {
 //   /*
 //    * Добавь статический метод `getSpecs(car)`,
 //    * который принимает объект-машину как параметр и выводит
@@ -55,24 +53,73 @@
 //    * но только в том случае если машина заведена!
 //    */
 //   drive(hours) {}
-// }
 
-// const mustang = new Car({ maxSpeed: 200, price: 2000 });
+class Car {
+    static getSpecs(car = {}) {
+        console.log(
+            `maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car.price}`,
+        );
+    }
 
-// mustang.turnOn();
-// mustang.accelerate(50);
-// mustang.drive(2);
+    constructor({ maxSpeed = 0, speed = 0, isOn = false, distance = 0, price = 0 } = {}) {
+        this.maxSpeed = maxSpeed;
+        this.speed = speed;
+        this.isOn = isOn;
+        this.distance = distance;
+        this._price = price;
+    }
+    get price() {
+        return this._price;
+    }
+    set price(newPrice) {
+        this._price = newPrice;
+    }
+    turnOn() {
+        this.isOn = true;
+    }
+    turnOff() {
+        this.isOn = false;
+        this.speed = 0;
+    }
+    accelerate(value) {
+        if (this.speed + value < this.maxSpeed) {
+            this.speed += value;
+        }
+    }
+    decelerate(value) {
+        if (this.speed - value > 0) {
+            this.speed -= value;
+        }
+    }
+    drive(hours) {
+        if (this.isOn === true) {
+            this.distance += hours * this.speed;
+        }
+    }
+}
 
-// Car.getSpecs(mustang);
-// // maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+const mustang = new Car({ maxSpeed: 200, price: 2000 });
 
-// mustang.decelerate(20);
-// mustang.drive(1);
-// mustang.turnOff();
+mustang.turnOn();
+mustang.accelerate(50);
+mustang.drive(2);
 
-// Car.getSpecs(mustang);
-// // maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+Car.getSpecs(mustang);
+// maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
 
-// console.log(mustang.price); // 2000
-// mustang.price = 4000;
-// console.log(mustang.price); // 4000
+mustang.decelerate(20);
+mustang.drive(1);
+mustang.turnOff();
+
+Car.getSpecs(mustang);
+// maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+
+console.log(mustang.price); // 2000
+mustang.price = 4000;
+console.log(mustang.price); // 4000
+
+// RESULT
+// maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+// maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+// 2000
+// 4000
